0f122c052ff39ac8a75feae4571702b21aca30a7
Laura <lmcsay@gmail.com>
2021-02-06 09:20:49 -0600

    Feat: Add items to accordion and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import Accordion from './components/Accordion'
 
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a frontend JavaScript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
 const App = () => {
     return (
-        <Accordion />
+        <Accordion items={items}/>
     )
 };
 
