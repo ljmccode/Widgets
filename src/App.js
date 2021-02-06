629af3208206a42fb0945180bf874e45f6521640
Laura <lmcsay@gmail.com>
2021-02-06 16:47:21 -0600

    Feat: Display selected color at top of dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -33,10 +33,17 @@ const options = [
     }
 ];
 
+
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <Dropdown options={options}/>
+            <Dropdown 
+                selected={selected} 
+                options={options}
+                onSelectedChange={setSelected}/>
         </div>
     )
 };
