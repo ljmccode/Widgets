0e9e6da10454358e540ca97a6fa4881f605996c1
Laura <lmcsay@gmail.com>
2021-02-07 11:19:05 -0600

    Feat: Create routes to different widgets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, {useState} from 'react';
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 
-// const items = [
-//     {
-//         title: 'What is React',
-//         content: 'React is a frontend JavaScript framework'
-//     },
-//     {
-//         title: 'Why use React?',
-//         content: 'React is a favorite JS library among engineers'
-//     },
-//     {
-//         title: 'How do you use React?',
-//         content: 'You use React by creating components'
-//     }
-// ]
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a frontend JavaScript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 
 const options = [
     {
@@ -34,16 +34,39 @@ const options = [
     }
 ];
 
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return <Accordion items={items}/>
+    }
+}
 
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return <Search />
+    }
+}
 
-const App = () => {
-    const [selected, setSelected] = useState(options[0]);
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return <Dropdown />
+    }
+}
+
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return <Translate />
+    }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
 
     return (
         <div style={{ margin: 50 }}>
-            <Translate />
+            {showAccordion()}
+            {showList()}
+            {showDropdown()}
+            {showTranslate()}
         </div>
     )
-};
-
-export default App;
+}
